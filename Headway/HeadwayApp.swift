5d876f11952437f9843fe84c9e719ee5accd5bb2
omar abozeid <omarabozeid.pro@gmail.com>
2025-02-01 06:44:45 -0800

    Initial Commit

---
+++ Headway/HeadwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayApp.swift
+//  Headway
+//
+//  Created by omar abozeid on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeadwayTests/HeadwayTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayTests.swift
+//  HeadwayTests
+//
+//  Created by omar abozeid on 01/02/2025.
+//
+
+import Testing
+@testable import Headway
+
+struct HeadwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
