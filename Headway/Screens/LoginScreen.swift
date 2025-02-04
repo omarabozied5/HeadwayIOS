bd65c57450cb0502065250474d5bcc4c9d974738
omar abozeid <omarabozeid.pro@gmail.com>
2025-02-04 04:52:39 -0800

    validate

--- Headway/Screens/LoginScreen.swift
+++ Headway/Screens/LoginScreen.swift
@@ -108,7 +108,7 @@ struct LoginScreen: View {
     }
     
     func isValidEmail(_ email: String) -> Bool {
-        let emailRegex = #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+        let emailRegex =  "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
     }
 
